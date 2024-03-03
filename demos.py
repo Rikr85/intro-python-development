83773e860a300548ee19de67abc6c7382bc49cf8
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-03 12:44:06 -0400

    Demo Strings Python

---
+++ demos.py
@@ -0,0 +1,7 @@
+# first_name = 'Christopher'
+# last_name = 'Harrison'
+
+first_name = input('Please enter your first name: ')
+last_name = input('Please enter your last name: ')
+# print(first_name + last_name)
+print('Hello, ' + first_name + ' ' + last_name.capitalize())
