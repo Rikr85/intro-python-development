79bd2b1c139614447732dcfdddb9812f25e50f46
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-03 12:28:56 -0400

    Using functions help us format strings when we save to files and databases

--- helloworld.py
+++ helloworld.py
@@ -2,15 +2,20 @@
 #y = x / 0
 # print('yay I did math')
 # Strings can be stored in variables 
-first_name = 'Christopher'
-print(first_name)
+# first_name = 'Christopher'
+# print(first_name)
 
-last_name = 'Harrison'
-print(first_name + last_name)
-print('Hello ' + first_name + ' ' + last_name)
+# last_name = 'Harrison'
+# print(first_name + last_name)
+# print('Hello ' + first_name + ' ' + last_name)
 
-sentence = 'The dog is named Sammy'
-print(sentence.upper())
-print(sentence.lower())
-print(sentence.capitalize())
-print(sentence.count('h'))
+# sentence = 'The dog is named Sammy'
+# print(sentence.upper())
+# print(sentence.lower())
+# print(sentence.capitalize())
+#print(sentence.count('h'))
+
+first_name = input('What is your first name? ')
+last_name = input('What is your last name?' )
+print ('Hello ' + first_name.capitalize() + ' ' \
+       + last_name.capitalize())
