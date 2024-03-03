399c8b29b650c3e51cafdcbc5b51ea244a803339
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-03 13:30:02 -0400

    Working with numbers

---
+++ numbers.py
@@ -0,0 +1,23 @@
+pi = 3.14159
+print(pi)
+
+first_num = 6
+second_num = 2
+print(first_num + second_num)
+print(first_num ** second_num)
+
+days_in_feb = 28
+print(str(days_in_feb) + ' days in February')
+
+first_num = '5'
+second_num = '6'
+print(first_num + second_num)
+
+first_num = input('Enter first number: ')
+second_num = input('Enter second number: ')
+print(first_num + second_num)
+
+print(int(first_num) + int(second_num))
+print(float(first_num) + float(second_num))
+
+# print(module(int(first_num) + int(second_num)))
