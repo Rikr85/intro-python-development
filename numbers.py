dc9c13b990b9878f079c0a5e450eeca6ef868e60
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-03 13:41:11 -0400

    Numbers in Code

--- numbers.py
+++ numbers.py
@@ -1,23 +1,36 @@
-pi = 3.14159
-print(pi)
+# pi = 3.14159
+# print(pi)
 
-first_num = 6
-second_num = 2
-print(first_num + second_num)
-print(first_num ** second_num)
+# first_num = 6
+# second_num = 2
+# print(first_num + second_num)
+# print(first_num ** second_num)
 
-days_in_feb = 28
-print(str(days_in_feb) + ' days in February')
+# days_in_feb = 28
+# print(str(days_in_feb) + ' days in February')
 
-first_num = '5'
-second_num = '6'
-print(first_num + second_num)
+# first_num = '5'
+# second_num = '6'
+# print(first_num + second_num)
 
-first_num = input('Enter first number: ')
-second_num = input('Enter second number: ')
-print(first_num + second_num)
+# first_num = input('Enter first number: ')
+# second_num = input('Enter second number: ')
+# print(first_num + second_num)
 
-print(int(first_num) + int(second_num))
-print(float(first_num) + float(second_num))
+# print(int(first_num) + int(second_num))
+# print(float(first_num) + float(second_num))
 
 # print(module(int(first_num) + int(second_num)))
+
+# first_num = 5
+# second_num = 6
+# print(first_num ** second_num)
+
+#Siempre los valores de entrada de un input son cadenas de caracteres string
+
+# first_num = input('Please enter a number: ')
+# second_num = input('Please enter another number: ')
+# print(int(first_num) + int (second_num))
+
+days_in_February = 28
+print(str(days_in_February) + ' Total days in February')
