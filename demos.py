98a970e88613934f9d2776a65994a11d740a26cd
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-03 13:06:32 -0400

    Custom string formatting

--- demos.py
+++ demos.py
@@ -1,7 +1,15 @@
-# first_name = 'Christopher'
-# last_name = 'Harrison'
+first_name = 'Christopher'
+last_name = 'Harrison'
 
-first_name = input('Please enter your first name: ')
-last_name = input('Please enter your last name: ')
-# print(first_name + last_name)
-print('Hello, ' + first_name + ' ' + last_name.capitalize())
+# first_name = input('Please enter your first name: ')
+# last_name = input('Please enter your last name: ')
+# # print(first_name + last_name)
+# print('Hello, ' + first_name + ' ' + last_name.capitalize())
+
+#Custom string formatting 
+output = 'Hello, ' + first_name + ' ' + last_name
+output = 'Hello, {}{}'.format(first_name, last_name)
+output = 'Hello, {0}{1}'.format(first_name, last_name)
+#Only available in Python 3  
+output = f'Hello, {first_name}{last_name}'
+print(output)
