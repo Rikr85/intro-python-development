bc03de5e6d27ccabb8746daca85ae2a46832486d
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-04 12:45:25 -0400

    Using timedelta objects to manipulate dates

--- dates.py
+++ dates.py
@@ -1,7 +1,19 @@
 #To get current date and time 
 #we need to use the datetime library
-from datetime import datetime
 
+from datetime import datetime
 current_date = datetime.now()
+
 #the now function returns a datetime object 
+
 print('Today is: ' + str(current_date))
+
+from datetime import datetime, timedelta
+today = datetime.now()
+print('Today is: ' + str(today))
+
+#timdelta is used to define a period of time
+
+one_day = timedelta(days=1)
+yesterday = today - one_day
+print('Yesterday was: ' + str(yesterday))
