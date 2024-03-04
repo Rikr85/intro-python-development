24913c5f5f7f356508874ae1069eb8bdf24db8e7
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-04 13:13:54 -0400

    Add exception handling in case the date entered is invald

--- dates.py
+++ dates.py
@@ -1,43 +1,43 @@
 #To get current date and time 
 #we need to use the datetime library
 
-from datetime import datetime
-current_date = datetime.now()
 
-#the now function returns a datetime object 
+# from datetime import datetime
+# current_date = datetime.now()
+# print('Today is: ' + str(current_date))
 
-print('Today is: ' + str(current_date))
 
-from datetime import datetime, timedelta
-today = datetime.now()
-print('Today is: ' + str(today))
 
-#timdelta is used to define a period of time
+# from datetime import datetime, timedelta
+# today = datetime.now()
+# print('Today is: ' + str(today))
+# one_day = timedelta(days=1)
+# yesterday = today - one_day
+# print('Yesterday was: ' + str(yesterday))
 
-one_day = timedelta(days=1)
-yesterday = today - one_day
-print('Yesterday was: ' + str(yesterday))
 
-from datetime import datetime
-current_date = datetime.now()
+# from datetime import datetime
+# current_date = datetime.now()
+# print('Day: ' + str(current_date.day))
+# print('Month: ' + str(current_date.month))
+# print('Year: ' + str(current_date.year))
 
-print('Day: ' + str(current_date.day))
-print('Month: ' + str(current_date.month))
-print('Year: ' + str(current_date.year))
 
+# from datetime import datetime
+# birthday = input('When is your birthday (dd/mm/yyyy)?')
+# birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
+# print('Birthday: ' + str(birthday_date))
+
+
+# from datetime import datetime, timedelta
+# birthday = input('When is your birthday (dd/mm/yyyy)?')
+# birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
+# print('Birsthday: ' + str(birthday_date))
+# one_day = timedelta(dasy=1)
+# birthday_eve = birthday_date - one_day
+# print('Day before birthday: ' + str(birthday_eve))
 
 from datetime import datetime
 birthday = input('When is your birthday (dd/mm/yyyy)?')
-
 birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
-
 print('Birthday: ' + str(birthday_date))
-
-
-from datetime import datetime, timedelta
-birthday = input('When is your birthday (dd/mm/yyyy)?')
-birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
-print('Birsthday: ' + str(birthday_date))
-one_day = timedelta(dasy=1)
-birthday_eve = birthday_date - one_day
-print('Day before birthday: ' + str(birthday_eve))
