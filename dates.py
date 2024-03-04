1b5bfd4cab41ddf1e727a49401b96a52f4d268fe
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-04 12:49:45 -0400

    Use date functions to control date formatting

--- dates.py
+++ dates.py
@@ -17,3 +17,10 @@ print('Today is: ' + str(today))
 one_day = timedelta(days=1)
 yesterday = today - one_day
 print('Yesterday was: ' + str(yesterday))
+
+from datetime import datetime
+current_date = datetime.now()
+
+print('Day: ' + str(current_date.day))
+print('Month: ' + str(current_date.month))
+print('Year: ' + str(current_date.year))
