527e950287fb68c02496f98a834978edd0f1ae8c
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-05 17:17:33 -0400

    Complex conditions

---
+++ complex_conditions.py
@@ -0,0 +1,9 @@
+# A student makes honour roll if their average is >= 75
+# and their lowest grade is not below 75
+
+gpa = float(input('What was your Grade Point Average? '))
+lowest_grade = float(input('What was your lowest grade?'))
+
+if gpa >= .85:
+    if lowest_grade >= .70:
+        print('You made the honour roll')
