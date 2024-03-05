aa859e385447a0eeb87264be672e0d8b9ea5b79a
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-05 14:44:57 -0400

    Conditional logic with else added

--- conditional-logic.py
+++ conditional-logic.py
@@ -1,6 +1,16 @@
-price = input('how much did you pay?')
+# price = input('how much did you pay?')
 
+# price = float(price)
+# if price >= 1.00:
+#     tax = .07
+#     print('Tax rate is: ' + str(tax))
+
+
+price = input('how much did you pay?')
 price = float(price)
 if price >= 1.00:
     tax = .07
+    print('Tax rate is: ' + str(tax))
+else:
+    tax = 0
     print('Tax rate is: ' + str(tax))
