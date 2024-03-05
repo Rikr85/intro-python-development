8ae04fb3c746a6f227de76dded22e31eff13ddf3
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-05 15:49:37 -0400

    Reemplazing operator or with the operator in

--- multiple-conditions.py
+++ multiple-conditions.py
@@ -22,10 +22,21 @@
 #     tax = 0.15
 # print(tax)
 
+# province = input('What province do you live in?')
+# tax = 0
+
+# if province == 'Alberta' or province == 'Nunavut':
+#     tax = 0.05
+# elif province == 'Ontario':
+#     tax = 0.13
+# else:
+#     tax = 0.15
+# print(tax)
+
 province = input('What province do you live in?')
 tax = 0
 
-if province == 'Alberta' or province == 'Nunavut':
+if province in('Alberta', 'Nunavut', 'Yukon'):
     tax = 0.05
 elif province == 'Ontario':
     tax = 0.13
