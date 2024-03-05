d93f9c1546104573821c13d33a320412679c04e9
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-05 16:02:18 -0400

    Conditional if with operator in and using lower function

--- nested_if.py
+++ nested_if.py
@@ -1,7 +1,22 @@
-country = input('What country do you live in?')
-province = input('What province/state do you live in?')
+# country = input('What country do you live in?')
+# province = input('What province/state do you live in?')
 
-if country == 'Canada':
+# if country == 'Canada':
+#     if province in ('Alberta',\
+#                     'Nunavut','Yukon'):
+#         tax = 0.05
+#     elif province == 'Ontario':
+#         tax = 0.13
+#     else:
+#         tax = 0.15
+# else:
+#     tax = 0.0
+# print(tax)
+
+country = input('What country do you live in?')
+
+if country.lower() == 'Canada':
+    province = input('What province/state do you live in?')
     if province in ('Alberta',\
                     'Nunavut','Yukon'):
         tax = 0.05
