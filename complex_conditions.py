b36771f569d05d2d452c45f88b7737a2a28284a8
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-05 17:20:41 -0400

    Complex conditio using and

--- complex_conditions.py
+++ complex_conditions.py
@@ -9,10 +9,18 @@
 #         print('You made the honour roll')
 
 
+# gpa = float(input('What was your Grade Point Average? '))
+# lowest_grade = input('What was your lowest grade?')
+# lowest_grade = float(lowest_grade)
+
+# if gpa >= .85:
+#     if lowest_grade >= .70:
+#         print('You made the honour roll')
+
+
 gpa = float(input('What was your Grade Point Average? '))
 lowest_grade = input('What was your lowest grade?')
 lowest_grade = float(lowest_grade)
 
-if gpa >= .85:
-    if lowest_grade >= .70:
+if gpa >= .85 and lowest_grade >= .70:
         print('You made the honour roll')
