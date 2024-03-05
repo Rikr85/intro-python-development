648e1d2adb5b8e90f60124097c199e19a6221feb
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-05 14:46:39 -0400

    Conditional logic optimized

--- conditional-logic.py
+++ conditional-logic.py
@@ -10,7 +10,6 @@ price = input('how much did you pay?')
 price = float(price)
 if price >= 1.00:
     tax = .07
-    print('Tax rate is: ' + str(tax))
 else:
     tax = 0
-    print('Tax rate is: ' + str(tax))
+print('Tax rate is: ' + str(tax))
