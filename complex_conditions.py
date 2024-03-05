20d155ec81e4c491452acf2fcfcaa410350a0018
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-05 17:19:09 -0400

    Complex conditions with validating

--- complex_conditions.py
+++ complex_conditions.py
@@ -1,8 +1,17 @@
 # A student makes honour roll if their average is >= 75
 # and their lowest grade is not below 75
 
+# gpa = float(input('What was your Grade Point Average? '))
+# lowest_grade = float(input('What was your lowest grade?'))
+
+# if gpa >= .85:
+#     if lowest_grade >= .70:
+#         print('You made the honour roll')
+
+
 gpa = float(input('What was your Grade Point Average? '))
-lowest_grade = float(input('What was your lowest grade?'))
+lowest_grade = input('What was your lowest grade?')
+lowest_grade = float(lowest_grade)
 
 if gpa >= .85:
     if lowest_grade >= .70:
