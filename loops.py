08d85bb57e0a9f9d2bf3780908d02fecc2447a8c
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-06 13:08:37 -0400

    Looping a number of times

--- loops.py
+++ loops.py
@@ -1,3 +1,7 @@
 # Loop through a collection
-for name in ['Christopher', 'Susan']:
-    print(name)
+# for name in ['Christopher', 'Susan']:
+#     print(name)
+
+# Looping a number of times
+for index in range(0, 2):
+    print(index)
