a58fcd56cbef803d7e990e3916c4d59322687201
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-07 08:43:27 -0400

    Using functions instead of repeating code

--- functions.py
+++ functions.py
@@ -1,15 +1,31 @@
+# --------------------------------------------------------------
 # Sometimes we copy and paste our code
-import datetime
+# import datetime
 # print timestamps to see how long sections of code
 # take to run
 
+# first_name = 'Susan'
+# print('task completed')
+# print(datetime.datetime.now())
+# print()
+
+# for x in range(0,10):
+#     print(x)
+# print('task completed')
+# print(datetime.datetime.now())
+# print()
+# --------------------------------------------------------------
+# Using functions instead of repeating code
+import datetime
+# Print the current time
+def print_time():
+    print('task completed')
+    print(datetime.datetime.now())
+    print()
+
 first_name = 'Susan'
-print('task completed')
-print(datetime.datetime.now())
-print()
+print_time()
 
 for x in range(0,10):
     print(x)
-print('task completed')
-print(datetime.datetime.now())
-print()
+print_time()
