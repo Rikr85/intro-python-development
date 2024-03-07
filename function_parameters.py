6efd45dbf780a1dbc47fe1648615e1ebccb538ee
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-07 15:45:25 -0400

    Using the named notation when calling functions for makes the code more readable

--- function_parameters.py
+++ function_parameters.py
@@ -35,13 +35,24 @@
 # print('Your initial is: ' + first_name_initial)
 # ----------------------------------------------------------
 # I can also assign the values to parameters by name when you call the function
-def get_initial(name, force_uppercase):
-    if force_uppercase:
-        initial = name[0:1].upper()
-    else:
-        initial = name[0:1]
-    return initial
-first_name = input('Enter your first name: ')
-first_name_initial = get_initial(force_uppercase=True, \
-                                 name=first_name)
-print('Your initial is: ' + first_name_initial)
+# def get_initial(name, force_uppercase):
+#     if force_uppercase:
+#         initial = name[0:1].upper()
+#     else:
+#         initial = name[0:1]
+#     return initial
+# first_name = input('Enter your first name: ')
+# first_name_initial = get_initial(force_uppercase=True, \
+#                                  name=first_name)
+# print('Your initial is: ' + first_name_initial)
+
+# ----------------------------------------------------------
+# Using the named notation when calling functions makes your code more readable
+def error_logger(error_code, erro_severity, log_to_db, \
+                 error_message, source_module):
+    print('oh no error: ' + error_message)
+first_number = 10
+second_number = 5
+if first_number > second_number:
+    # error_logger(45, 1, True, 'Second number greater than first', 'my_math_method')
+    error_logger(error_code=45, erro_severity=1, log_to_db=True, error_message='Second number greater than first', source_module='my_math_method')
