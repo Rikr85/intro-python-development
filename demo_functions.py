8f9848bab00957054dcfff84ef0125986a73f30f
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-07 11:10:07 -0400

    using functions correctly

--- demo_functions.py
+++ demo_functions.py
@@ -1,15 +1,31 @@
-import datetime
+# ---------------------------------------------------------------
+# import datetime
 
-# print timestamps to see how long sections of code
-# take to run
+# # print timestamps to see how long sections of code
+# # take to run
+
+# first_name = 'Susan'
+# print('task completed')
+# print(datetime.datetime.now())
+# print()
+
+# for x in range(0,10):
+#     print(x)
+# print('task completed')
+# print(datetime.datetime.now())
+# print()
+
+# ---------------------------------------------------------------
+from datetime import datetime
+# Function to print current date and time
+def print_time(task_name):
+    print(task_name)
+    print(datetime.now())
+    print()
 
 first_name = 'Susan'
-print('task completed')
-print(datetime.datetime.now())
-print()
+print_time('printed first name')
 
 for x in range(0,10):
     print(x)
-print('task completed')
-print(datetime.datetime.now())
-print()
+print_time('completed for loop')
