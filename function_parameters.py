eac519729a8b0d5624ab6e1c18f11d1b655c7c2c
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-07 15:22:15 -0400

    Function can accepte inicialized parameter

--- function_parameters.py
+++ function_parameters.py
@@ -1,7 +1,22 @@
-def get_initial(name):
-    initial = name[0:1].upper()
+# ----------------------------------------------------------
+# def get_initial(name):
+#     initial = name[0:1].upper()
+#     return initial
+# first_name = input('Enter your first name: ')
+# first_name_initial = get_initial(first_name)
+
+# print('Your initial is: ' + first_name_initial)
+
+# ----------------------------------------------------------
+# Functions can accept multiple parameters
+def get_initial(name, force_uppercase):
+    if force_uppercase:
+        initial = name[0:1].upper()
+    else:
+        initial = name[0:1]
     return initial
+
 first_name = input('Enter your first name: ')
-first_name_initial = get_initial(first_name)
+first_name_initial = get_initial(first_name, False)
 
 print('Your initial is: ' + first_name_initial)
