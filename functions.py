8b1d44679aebed111507fcb4a608d63c49486235
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-07 08:49:46 -0400

    Pass the task name as a parameter

--- functions.py
+++ functions.py
@@ -16,16 +16,32 @@
 # print()
 # --------------------------------------------------------------
 # Using functions instead of repeating code
-import datetime
-# Print the current time
-def print_time():
-    print('task completed')
-    print(datetime.datetime.now())
+# import datetime
+# # Print the current time
+# def print_time():
+#     print('task completed')
+#     print(datetime.datetime.now())
+#     print()
+
+# first_name = 'Susan'
+# print_time()
+
+# for x in range(0,10):
+#     print(x)
+# print_time()
+# --------------------------------------------------------------
+# Pass the task name as a parameter
+from datetime import datetime
+
+# Print the current time and task name
+def print_time(task_name):
+    print(task_name)
+    print(datetime.now())
     print()
 
 first_name = 'Susan'
-print_time()
+print_time('first name assigned')
 
 for x in range(0,10):
     print(x)
-print_time()
+print_time('loop completed')
