eb5091a0df66be3d52d5d62be5189a7ea18bd31c
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-08 23:24:59 -0400

    Parametrized funcitons with many parameters

--- parametrized_functions.py
+++ parametrized_functions.py
@@ -1,10 +1,24 @@
+# ---------------------------------------------------------------
 # This funcion will take a name and return the
-# # first letter of the name
-def get_initial(name):
-    initial = name[0:1].upper()
+# # # first letter of the name
+# def get_initial(name):
+#     initial = name[0:1].upper()
+#     return initial
+
+# # Ask for someone's name and return the initial
+# first_name = input('Enter your first name: ')
+# first_name_initial = get_initial(first_name)
+# print('Your initial is: ' + first_name_initial)
+
+# ---------------------------------------------------------------
+# Another parametrized functions
+def get_initial(name, force_uppercase):
+    if force_uppercase:
+        initial = name[0:1].upper()
+    else:
+        initial = name[0:1]
     return initial
 
-# Ask for someone's name and return the initial
 first_name = input('Enter your first name: ')
-first_name_initial = get_initial(first_name)
+first_name_initial = get_initial(first_name, True)
 print('Your initial is: ' + first_name_initial)
