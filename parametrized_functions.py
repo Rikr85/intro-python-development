61f10360c65c1020c00785fe5a0efb1c4fc585e4
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-08 23:32:39 -0400

    Parametrized function with default value

--- parametrized_functions.py
+++ parametrized_functions.py
@@ -12,13 +12,24 @@
 
 # ---------------------------------------------------------------
 # Another parametrized functions
-def get_initial(name, force_uppercase):
+# def get_initial(name, force_uppercase):
+#     if force_uppercase:
+#         initial = name[0:1].upper()
+#     else:
+#         initial = name[0:1]
+#     return initial
+
+# first_name = input('Enter your first name: ')
+# first_name_initial = get_initial(first_name, True)
+# print('Your initial is: ' + first_name_initial)
+# ---------------------------------------------------------------
+def get_initial(name, force_uppercase=True):
     if force_uppercase:
         initial = name[0:1].upper()
     else:
         initial = name[0:1]
     return initial
-
 first_name = input('Enter your first name: ')
-first_name_initial = get_initial(first_name, True)
+first_name_initial = get_initial(first_name)
+
 print('Your initial is: ' + first_name_initial)
