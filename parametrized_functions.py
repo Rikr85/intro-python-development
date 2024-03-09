2c8f268331bbef6a52d0f0edca78ed350a8ec59d
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-08 23:37:21 -0400

    Adding parametrized funcions with notation names

--- parametrized_functions.py
+++ parametrized_functions.py
@@ -30,6 +30,7 @@ def get_initial(name, force_uppercase=True):
         initial = name[0:1]
     return initial
 first_name = input('Enter your first name: ')
-first_name_initial = get_initial(first_name)
+# first_name_initial = get_initial(first_name)
+first_name_initial = get_initial(force_uppercase=False, name=first_name)
 
 print('Your initial is: ' + first_name_initial)
