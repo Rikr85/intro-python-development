fd0879362dec80861198a800076885b9b47ed239
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-10 16:33:27 -0400

    Working with packages

--- demos.py
+++ demos.py
@@ -1,15 +1,20 @@
-first_name = 'Christopher'
-last_name = 'Harrison'
+# first_name = 'Christopher'
+# last_name = 'Harrison'
 
-# first_name = input('Please enter your first name: ')
-# last_name = input('Please enter your last name: ')
-# # print(first_name + last_name)
-# print('Hello, ' + first_name + ' ' + last_name.capitalize())
+# # first_name = input('Please enter your first name: ')
+# # last_name = input('Please enter your last name: ')
+# # # print(first_name + last_name)
+# # print('Hello, ' + first_name + ' ' + last_name.capitalize())
 
-#Custom string formatting 
-output = 'Hello, ' + first_name + ' ' + last_name
-output = 'Hello, {}{}'.format(first_name, last_name)
-output = 'Hello, {0}{1}'.format(first_name, last_name)
-#Only available in Python 3  
-output = f'Hello, {first_name}{last_name}'
-print(output)
+# #Custom string formatting 
+# output = 'Hello, ' + first_name + ' ' + last_name
+# output = 'Hello, {}{}'.format(first_name, last_name)
+# output = 'Hello, {0}{1}'.format(first_name, last_name)
+# #Only available in Python 3  
+# output = f'Hello, {first_name}{last_name}'
+# print(output)
+# import helpers
+# helpers.display('Sample message test', True)
+
+from helpers import display
+display('Sample message', True)
--- helpers.py
+++ helpers.py
@@ -1,27 +1,32 @@
-# helpers.py
+# # helpers.py
+# def display(message, is_warning=False):
+#     if is_warning:
+#         print('Warning!!')
+#     print(message)
+
+# # import module as namespace
+# import helpers 
+# helpers.display('Not a warning')
+
+# #import all into current namespace
+# from helpers import *
+# display('Not a warning')
+
+# # import specific items into current namespace
+# from helpers import display
+# display('Not a warning')
+
+# # Installing packages
+# # Install an individual package
+# pip install colorama
+
+# # Install from a list of packages
+# pip install -r requirements.txt 
+
+# # requirements.txt
+# colorama
+
 def display(message, is_warning=False):
     if is_warning:
-        print('Warning!!')
+        print('This is a warning')
     print(message)
-
-# import module as namespace
-import helpers 
-helpers.display('Not a warning')
-
-#import all into current namespace
-from helpers import *
-display('Not a warning')
-
-# import specific items into current namespace
-from helpers import display
-display('Not a warning')
-
-# Installing packages
-# Install an individual package
-pip install colorama
-
-# Install from a list of packages
-pip install -r requirements.txt 
-
-# requirements.txt
-colorama
