ef23527bb3d0235c9ce822e1230813d792bba3a5
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-10 16:46:16 -0400

    Updates packages

--- packages.py
+++ packages.py
@@ -1,46 +1,46 @@
-# ----------------------------------------------------
-# Installing packages
-# Install an individual package
-pip install colorama
+# # ----------------------------------------------------
+# # Installing packages
+# # Install an individual package
+# pip install colorama
 
-# Install from a list of packages
-pip install -r requirements.txt
+# # Install from a list of packages
+# pip install -r requirements.txt
 
-# requirements.txt
-colorama
+# # requirements.txt
+# colorama
 
-# ----------------------------------------------------
-# Creating a virtual environments
-# Install virtual environment
-pip install virtualenv
+# # ----------------------------------------------------
+# # Creating a virtual environments
+# # Install virtual environment
+# pip install virtualenv
 
-# Windows systems
-python -m venv <folder_name>
+# # Windows systems
+# python -m venv <folder_name>
+
+# # OSX/Linux (bash)
+# virtualenv <folder_name>
+
+# # ----------------------------------------------------
+# # Using virtual environments
+# # Windows systems
+# # cmd.exe
+# <folder_name>\Scripts\Activate.bat
+# # Powershell
+# <folder_name>\Scripts\Activate.ps1
+# # bash shell
+# ../<folder_name>\Scripts\Activate
 
 # OSX/Linux (bash)
-virtualenv <folder_name>
+# <folder_name>/Scripts/Activate
 
-# ----------------------------------------------------
-# Using virtual environments
-# Windows systems
-# cmd.exe
-<folder_name>\Scripts\Activate.bat
-# Powershell
-<folder_name>\Scripts\Activate.ps1
-# bash shell
-../<folder_name>\Scripts\Activate
+# # ----------------------------------------------------
+# # Installing packages in a virtual environment
+# # Install an individual package
+# pip install colorama 
 
-OSX/Linux (bash)
-<folder_name>/Scripts/Activate
+# # Install from a list of packages
+# pip install -r requirements.txt 
 
-# ----------------------------------------------------
-# Installing packages in a virtual environment
-# Install an individual package
-pip install colorama 
-
-# Install from a list of packages
-pip install -r requirements.txt 
-
-# requirements.txt
-colorama
+# # requirements.txt
+# colorama
 
