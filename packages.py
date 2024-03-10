047a94fb3bdbae61ca5076b50ffb0ab876d90122
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-10 01:32:01 -0400

    Installing packages in a virtual enviroment

--- packages.py
+++ packages.py
@@ -33,5 +33,14 @@ virtualenv <folder_name>
 OSX/Linux (bash)
 <folder_name>/Scripts/Activate
 
+# ----------------------------------------------------
+# Installing packages in a virtual environment
+# Install an individual package
+pip install colorama 
 
+# Install from a list of packages
+pip install -r requirements.txt 
+
+# requirements.txt
+colorama
 
