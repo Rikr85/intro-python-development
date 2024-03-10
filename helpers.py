dc478cc4ca2c6505a0127734c90ff11e0cf8032e
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-09 20:10:02 -0400

    Importing a module

--- helpers.py
+++ helpers.py
@@ -3,3 +3,15 @@ def display(message, is_warning=False):
     if is_warning:
         print('Warning!!')
     print(message)
+
+# import module as namespace
+import helpers 
+helpers.display('Not a warning')
+
+#import all into current namespace
+from helpers import *
+display('Not a warning')
+
+# import specific items into current namespace
+from helpers import display
+display('Not a warning')
