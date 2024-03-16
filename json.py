576b5ad31253534d27ce71f2aed673aced51d452
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-16 18:05:44 -0400

    to retrieve a value from a specify the keyname and index position of the value to retrieve

--- json.py
+++ json.py
@@ -1,5 +1,9 @@
 # results = {"request":"afdasfasf-qwerqwr-qwerqwer-qwerqwre"}
 # print(results['request'])
 
-results = {"color":{"dominantColorForeground":"White", "dominantColorBackground":"White", "accentColor":["white"]}}
-print(results['color']['dominantColorForeground'])
+# results = {"color":{"dominantColorForeground":"White", "dominantColorBackground":"White", "accentColor":["white"]}}
+# print(results['color']['dominantColorForeground'])
+
+results = {"description":{"tag":{"bear","polar","animal","outdoor","water","white","walking","snow","standing"}}}
+
+print(results['description']['tags'][0])
