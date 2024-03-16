d522f462aafed17bb28f1a17e84b5059cc7bfa5e
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-16 18:13:01 -0400

    Using a loop to retrieve all values

--- json.py
+++ json.py
@@ -4,6 +4,10 @@
 # results = {"color":{"dominantColorForeground":"White", "dominantColorBackground":"White", "accentColor":["white"]}}
 # print(results['color']['dominantColorForeground'])
 
-results = {"description":{"tag":{"bear","polar","animal","outdoor","water","white","walking","snow","standing"}}}
+# results = {"description":{"tag":{"bear","polar","animal","outdoor","water","white","walking","snow","standing"}}}
+# print(results['description']['tags'][0])
 
-print(results['description']['tags'][0])
+results = {"description":{"tags":["bear","polar","animal","mammal","outdoor","water","white","large","walking","snow"],"captions":[{"text":"a large white polar bear walking in the water","confidence":0.7446564664}]}}
+
+for item in results['description']['tags']:
+    print(item)
