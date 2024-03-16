8ead623ccb4366983c4d82a86f11b0c88f0fdd80
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-16 18:26:30 -0400

    Convert dictionary to JSON object

--- json.py
+++ json.py
@@ -7,7 +7,17 @@
 # results = {"description":{"tag":{"bear","polar","animal","outdoor","water","white","walking","snow","standing"}}}
 # print(results['description']['tags'][0])
 
-results = {"description":{"tags":["bear","polar","animal","mammal","outdoor","water","white","large","walking","snow"],"captions":[{"text":"a large white polar bear walking in the water","confidence":0.7446564664}]}}
 
-for item in results['description']['tags']:
-    print(item)
+
+# results = {"description":{"tags":["bear","polar","animal","mammal","outdoor","water","white","large","walking","snow"],"captions":[{"text":"a large white polar bear walking in the water","confidence":0.7446564664}]}}
+# for item in results['description']['tags']:
+#     print(item)
+
+# Create a dictionary object
+person_dict = {'first':'Christopher', 'last':'Harrison'}
+# Add additional key pairs as needed to dictionary
+person_dict['City']='Seattle'
+
+# Convert dictionary to JSON object
+person_dict = json.dumps(person_dict)
+print(person_dict)
