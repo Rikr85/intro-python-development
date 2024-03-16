1fb7fb5015adb6b7ae77ace36d0f96df12aa4114
Rikr85 <129728638+Rikr85@users.noreply.github.com>
2024-03-16 18:01:31 -0400

    Request a value from a specify the key name and the subkey name

--- json.py
+++ json.py
@@ -1,3 +1,5 @@
-results = {"request":"afdasfasf-qwerqwr-qwerqwer-qwerqwre"}
+# results = {"request":"afdasfasf-qwerqwr-qwerqwer-qwerqwre"}
+# print(results['request'])
 
-print(results['request'])
+results = {"color":{"dominantColorForeground":"White", "dominantColorBackground":"White", "accentColor":["white"]}}
+print(results['color']['dominantColorForeground'])
